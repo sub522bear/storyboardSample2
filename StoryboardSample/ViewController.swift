a82329af534ec299ec52f65eac759d2fa208c9ce
soribada <sd@isub-mac-mini.local>
2016-07-28 18:01:22 +0900

    aaa

--- StoryboardSample/ViewController.swift
+++ StoryboardSample/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let str = "storyboard sample"
+        let str = "storyboard sample  aa"
         print("str : \(str)")
     }
 
