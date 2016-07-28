bde4079fdcb126d26d743c85869f44e65eb5cb15
soribada <sd@isub-mac-mini.local>
2016-07-28 16:58:42 +0900

    Initial Commit

---
+++ StoryboardSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardSample
+//
+//  Created by sub on 2016. 7. 28..
+//  Copyright © 2016년 soribada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
